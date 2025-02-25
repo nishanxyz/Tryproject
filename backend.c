af735732d18856c24af626d091c88bd4ca4c2119
abhi__yang <sharma.abhiyan9866@gmail.com>
2025-02-25 22:06:46 +0545

    Writing the Outline to work on

--- backend.c
+++ backend.c
@@ -1,55 +1,86 @@
-#include<stdio.h>
-void LoginPage();
-void adminlogin();//function to call the login page
-void menuui();
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+void main_page();
+void adminlogin(); // function to call the login page
+void adminmenu();
 
-
-
-void LoginPage()  //Login preface
+void main_page() // Login preface
 {
     int opt;
-    printf("\n\t1.Admin Login\n\t2.Member Login \n\t3.New Member Registration\n");
-    again:
+    printf("\n\t1.Admin Login\n\t2.Member Login \n\t3.New Member Registration\n\t4.Customer\n");
+again:
+
     printf("Login:  ");
-    scanf("%d",&opt);
-  
- switch (opt)
- {
- case 1:
-     adminlogin();
-     break;
- case 2:
-     // memberlogin();
- default:
- printf("Invalid input\n");
- goto again;
-     break;
- }
-}
-void adminlogin() //admin login page
-{
-    char username[10];                              
-    char password[10];                              
-    printf("         Username: ");
-    scanf("%s",username);
-    printf("         Password: ");
-    scanf("%s",password);
-    if ((strcmp(username,"admin")==0) && (strcmp(password,"admin")==0))
+    scanf("%d", &opt);
+
+    switch (opt)
     {
-         menuui();
+    case 1:
+        adminlogin();
+        break;
+    case 2:
+        // memberlogin();
+        break;
+    case 3:
+        // newregistration();
+        break;
+    case 4:
+        // customer();
+        break;
+    default:
+        printf("Invalid input\n");
+        system("cls");
+        goto again;
+        break;
+    }
+}
+void adminlogin() // admin login page
+{
+    char username[10];
+    char password[10];
+    char a;
+top:
+    printf("         Username: ");
+    scanf("%s", username);
+    printf("         Password: ");
+    scanf("%s", password);
+    if ((strcmp(username, "admin") == 0) && (strcmp(password, "admin") == 0))
+    {
+        adminmenu();
     }
     else
     {
-        printf("wrong credentials");
+        printf("----------Wrong credentials--------");
+        printf("\n\tWant to try again?[Y/N]  ");
+        scanf("%c", a);
+        if (toupper(a) == 'Y')
+        {
+            goto top;
+        }
     }
 }
 
-void menuui() // user menu 
+void adminmenu() // adminmenu
 {
-    printf("What do you want to do?\n"
-           "1. About Books List\n"
-           "2. Dealer List\n"
-           "3. Customer List\n"
-           "4. Maintenance Record\n"
-           "5. Distributor and dealer list\n");
+    int opt;
+    system("cls");
+    printf("\n-------Welcome to admin page:-------\n\t1.Add new book\n\t2.Available Books\n\t3.Total sales\n\t4.Members List");
+    printf("\n\t Option:-- ");
+    scanf("%d", &opt);
+    switch (opt)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+
+    default:
+        break;
+    }
 }
---
+++ book_store_management.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "backend.c"
+int main(void)
+{
+    printf("\t-------MAIIN MENU-------\n\tWelcome to the Store\n\t Your are:---\n");
+    main_page();
+    return 0;
+}
