d7e8ed5537be10f06cc2b3ac99265b36fd9452be
abhi__yang <sharma.abhiyan9866@gmail.com>
2025-02-26 12:58:33 +0545

    Making an sturucture acessible everywhere

---
+++ books.h
@@ -0,0 +1,13 @@
+// books.h
+#ifndef BOOKS_H
+#define BOOKS_H
+
+struct books {
+    char book[100];
+    char author[100];
+    char genre[100];
+    int price;
+    // Add more fields as needed
+};
+
+#endif // BOOKS_H
